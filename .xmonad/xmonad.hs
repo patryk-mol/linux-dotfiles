0ee9ce570bd287649c5bdc590af62db323e18b55
Patryk Mól <patryk.mol91@gmail.com>
2021-07-23 20:39:27 +0200

    Update configs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,67 +2,171 @@ import XMonad
 import XMonad.Actions.CopyWindow
 import XMonad.Actions.WithAll
 import XMonad.Actions.Promote
+import XMonad.Actions.CycleWS
+import XMonad.Actions.MouseResize
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.Run
 import XMonad.Util.SpawnOnce
 import XMonad.Util.EZConfig
+import XMonad.Util.NamedScratchpad
 import XMonad.Layout.LayoutModifier
+import XMonad.Layout.LimitWindows
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.Renamed
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.SimplestFloat
 import XMonad.Layout.Spacing
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.WindowArranger
+import XMonad.Layout.WindowNavigation
+import qualified Data.Map as M
 import qualified XMonad.StackSet as W
+import qualified XMonad.Layout.MultiToggle as MT
+import qualified XMonad.Layout.ToggleLayouts as T
 import System.Exit
 
 myTerminal = "alacritty"
+myBrowser = "firefox"
+myEmailClient = "thunderbird"
+myCalendar = "gnome-calendar"
+myVisualEditor = "subl"
+myGitClient = "gitg"
+myCalculator = "galculator"
 myBorderWidth = 1
-mySpacing = 4
 myModMask = mod4Mask
-myWorkspaces = ["1", "2", "3"]
-myNormalBorderColor = "#bbbbbb"
+myWorkspaces = ["1", "2", "3", "4", "5"]
+myNormalBorderColor = "#000000"
 myFocusedBorderColor = "#00ff00"
 
-myFocusFollowsMouse :: Bool
+rect = W.RationalRect l t w h
+               where
+                 h = 0.25
+                 w = 0.25
+                 t = (1.0 -h) /2
+                 l = (1.0 -w) /2 
+
+
 myFocusFollowsMouse = True
-myClickJustFocuses :: Bool
 myClickJustFocuses = False
 
-applySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
-applySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
+windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
 
-myLayout = avoidStruts (tiled ||| Mirror tiled ||| Full)
-  where
-     tiled   = applySpacing 4 $ Tall nmaster delta ratio
-     nmaster = 1
-     ratio   = 1/2
-     delta   = 3/100
+applySpacing i = spacingRaw True (Border i i i i) True (Border i i i i) True
+
+tall     = renamed [Replace "tall"] $ avoidStruts $ smartBorders $ windowNavigation $ limitWindows 5 $ applySpacing 4 $ ResizableTall 1 (3/100) (1/2) []
+threeCol = renamed [Replace "threeCol"] $ avoidStruts $ smartBorders $ windowNavigation $ limitWindows 7 $ ThreeCol 1 (3/100) (1/2)
+monocle  = renamed [Replace "monocle"] $ avoidStruts $ smartBorders $ windowNavigation $ limitWindows 20 Full
+floats = renamed [Replace "floats"] $ avoidStruts $ smartBorders $ limitWindows 20 simplestFloat
+full = renamed [Replace "full"] $ smartBorders $ windowNavigation $ limitWindows 1 Full
+
+myLayoutHook = mouseResize $ windowArrange $ T.toggleLayouts floats
+               $ onWorkspaces ["1"] full $ mkToggle (NBFULL ?? NOBORDERS ?? EOT) myDefaultLayout
+             where
+               myDefaultLayout = withBorder myBorderWidth tall
+                                 ||| noBorders monocle
+                                 ||| floats
+                                 ||| threeCol
+                                 ||| noBorders full
+
+myManageHook = composeAll
+     [ className =? "confirm"         --> doFloat
+     , className =? "file_progress"   --> doFloat
+     , className =? "dialog"          --> doFloat
+     , className =? "download"        --> doFloat
+     , className =? "error"           --> doFloat
+     , className =? "Gimp"            --> doFloat
+     , className =? "notification"    --> doFloat
+     , className =? "pinentry-gtk-2"  --> doFloat
+     , className =? "splash"          --> doFloat
+     , className =? "toolbar"         --> doFloat
+     , className =? "Bitwarden"       --> doFloat
+     , className =? "Galculator"      --> doRectFloat rect
+     , (className =? "firefox" <&&> resource =? "Dialog") --> doFloat
+     , isFullscreen -->  doFullFloat
+     ]
 
 myStartupHook = do
-    spawnOnce "xrandr -s 1600x900"
-    spawnOnce "nitrogen --restore &"
+    spawnOnce "lxsession &"
+    spawnOnce "~/.fehbg &"
     spawnOnce "picom &"
+    spawnOnce "volumeicon &"
+    spawnOnce "nm-applet &"
+    spawnOnce "cbatticon -i notification &"
+    spawnOnce "dunst &"
+    spawnOnce "xfce4-power-manager --daemon"
+    spawnOnce "optimus-manager-qt &"
+    spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 0 --transparent true --alpha 0 --tint 0x282c34  --height 22 &"
 
 myKeybindings = [
+-- general keybinds
     ("M-C-r", spawn "xmonad --recompile"),
     ("M-r", spawn "xmonad --restart"),
     ("M-q", kill1),
     ("M-C-q", killAll),
     ("M-S-l", io exitSuccess),
+-- app hotkeys
     ("M-<Return>", spawn (myTerminal)),
-    ("M-<Space>", spawn "dmenu_run -i -p \"Run: \""),
+    ("M-<Space>", spawn "dmenu_run -i -p \"Run:\""),
+    ("M-a", spawn (myCalculator)),
+    ("M-s", spawn (myVisualEditor)),
+    ("M-e", spawn (myEmailClient)),
+    ("M-c", spawn (myCalendar)),
+    ("M-g", spawn (myGitClient)),
+    ("M-b", spawn (myBrowser)),
+    ("M-S-b", spawn (myBrowser ++ " -private-window")),
+-- window manipulation (move/resize/shift)
     ("M-S-m", promote),
     ("M-S-j", windows W.swapDown),
-    ("M-S-k", windows W.swapUp)
-    ]
+    ("M-S-k", windows W.swapUp),
+    ("M-C-h", sendMessage Shrink),
+    ("M-C-j", sendMessage MirrorShrink),
+    ("M-C-k", sendMessage MirrorExpand),
+    ("M-C-l", sendMessage Expand),
+    ("M-S-.", shiftToNext >> nextWS),
+    ("M-S-,", shiftToPrev >> prevWS),
+-- change focused screen
+    ("M-.", nextScreen),
+    ("M-,", prevScreen),
+-- change layout
+    ("M-<Tab>", sendMessage NextLayout),
+-- toggle fullscreen
+    ("M-f", sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts),
+-- float/tile windows
+    ("M-S-f", withFocused toggleFloat),
+    ("M-S-t", sinkAll)
+    ] where
+            toggleFloat w = windows (\s -> if M.member w (W.floating s)
+                            then W.sink w s
+                            else (W.float w rect s))
 
 main = do
-   xmproc <- spawnPipe "xmobar -x 0 /home/xxx/.config/xmobar/xmobarrc"
-   xmonad $ docks defaults
-
-defaults = def {
-    terminal = myTerminal,
-    workspaces = myWorkspaces,
-    borderWidth = myBorderWidth,
-    modMask = myModMask,
-    normalBorderColor = myNormalBorderColor,
-    focusedBorderColor = myFocusedBorderColor,
-    layoutHook = myLayout,
-    startupHook = myStartupHook
+    xmproc <- spawnPipe "xmobar -x 0 /home/patryk/.config/xmobar/1920x1080rc"
+    xmonad $ docks $ ewmh def {
+        manageHook = myManageHook <+> manageDocks,
+        terminal = myTerminal,
+        workspaces = myWorkspaces,
+        borderWidth = myBorderWidth,
+        modMask = myModMask,
+        normalBorderColor = myNormalBorderColor,
+        focusedBorderColor = myFocusedBorderColor,
+        layoutHook = myLayoutHook,
+        startupHook = myStartupHook,
+        logHook = dynamicLogWithPP $ namedScratchpadFilterOutWorkspacePP $ xmobarPP
+            { ppOutput = \x -> hPutStrLn xmproc x
+            , ppCurrent = xmobarColor "#00ff00" ""                -- Current workspace
+            , ppVisible = xmobarColor "#e5951d" ""                -- Visible but not current workspace
+            , ppHidden = xmobarColor "#82aaff" ""                 -- Hidden workspaces
+            , ppHiddenNoWindows = xmobarColor "#c792ea" ""        -- Hidden workspaces (no windows)
+            , ppTitle = xmobarColor "#b3afc2" "" . shorten 80     -- Title of active window
+            , ppSep =  "<fc=#666666> <fn=1>|</fn> </fc>"          -- Separator character
+            , ppUrgent = xmobarColor "#c45500" "" . wrap "!" "!"  -- Urgent workspace
+            , ppExtras  = [windowCount]                           -- # of windows current workspace
+            , ppOrder  = \(ws:l:_:ex) -> [ws,l]++ex               -- order of things in xmobar
+            }
 } `additionalKeysP` myKeybindings
--- .zshrc
+++ .zshrc
@@ -1,3 +1,10 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
@@ -8,7 +15,7 @@ export ZSH="/home/patryk/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="agnoster-custom"
+ZSH_THEME="agnoster"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -100,4 +107,14 @@ source $ZSH/oh-my-zsh.sh
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+
+export EDITOR=nano
+export VISUAL=subl
+
 alias dotfiles='/usr/bin/git --git-dir=$HOME/projects/linux-dotfiles/ --work-tree=$HOME'
+alias ls=exa
+alias lsl='exa -lh'
+alias lsal='exa -alh'
