39c2f5caa2eec50a7ee412607366c325b188fc74
Patryk Mól <patryk.mol91@gmail.com>
2022-08-18 18:42:41 +0200

    Update setup script

--- setup.sh
+++ setup.sh
@@ -27,3 +27,15 @@ sudo sh build_theme.sh -c Monochrome -t /usr/share/themes/Monochrome
 mkdir -p ~/.config/ulauncher/user-themes
 git clone git@github.com:patryk-mol/ulauncher-monochrome-theme.git ~/.config/ulauncher/user-themes/monochrome
 
+
+# Install plugins for Neovim
+
+echo "\n\nInstalling Neovim plugins...\n\n"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+echo "\n\nAfter starting Neovim exectue following commands:\n"
+echo ": PlugInstall\n"
+echo ": TSInstall bash c cpp css dockerfile gitignore json python swift yaml\n"
+echo "\n"
+
