30fce0c8b724c097931190fc50d0eda1408ef036
Patryk Mól <patryk-mol@users.noreply.github.com>
2023-07-19 14:24:13 +0200

    Update .setup.sh

--- .setup.sh
+++ .setup.sh
@@ -4,7 +4,7 @@
 
 printf "Applying dotfiles...\n\n"
 
-git clone --bare git@github.com:patryk-mol/linux-dotfiles.git ./projects/linux-dotfiles
+git clone --bare https://github.com/patryk-mol/linux-dotfiles.git ./projects/linux-dotfiles
 git --git-dir=$HOME/projects/linux-dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 git --git-dir=$HOME/projects/linux-dotfiles/ --work-tree=$HOME reset --hard
 
